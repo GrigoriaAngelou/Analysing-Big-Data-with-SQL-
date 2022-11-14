c86c43f1d6aa7520e504b2226e037321a4faa07e
Grigoria Angelou <103663915+GrigoriaAngelou@users.noreply.github.com>
2022-11-14 19:16:11 +0200

    Add files via upload

---
+++ More advanced SQL queries/Challenge, The wordiest author.sql
@@ -0,0 +1,77 @@
+CREATE TABLE artists (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    country TEXT,
+    genre TEXT);
+
+INSERT INTO artists (name, country, genre)
+    VALUES ("Taylor Swift", "US", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Led Zeppelin", "US", "Hard rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("ABBA", "Sweden", "Disco");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Queen", "UK", "Rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Celine Dion", "Canada", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Meatloaf", "US", "Hard rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Garth Brooks", "US", "Country");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Shania Twain", "Canada", "Country");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Rihanna", "US", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Guns N' Roses", "US", "Hard rock");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Gloria Estefan", "US", "Pop");
+INSERT INTO artists (name, country, genre)
+    VALUES ("Bob Marley", "Jamaica", "Reggae");
+
+CREATE TABLE songs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist TEXT,
+    title TEXT);
+
+INSERT INTO songs (artist, title)
+    VALUES ("Taylor Swift", "Shake it off");
+INSERT INTO songs (artist, title)
+    VALUES ("Rihanna", "Stay");
+INSERT INTO songs (artist, title)
+    VALUES ("Celine Dion", "My heart will go on");
+INSERT INTO songs (artist, title)
+    VALUES ("Celine Dion", "A new day has come");
+INSERT INTO songs (artist, title)
+    VALUES ("Shania Twain", "Party for two");
+INSERT INTO songs (artist, title)
+    VALUES ("Gloria Estefan", "Conga");
+INSERT INTO songs (artist, title)
+    VALUES ("Led Zeppelin", "Stairway to heaven");
+INSERT INTO songs (artist, title)
+    VALUES ("ABBA", "Mamma mia");
+INSERT INTO songs (artist, title)
+    VALUES ("Queen", "Bicycle Race");
+INSERT INTO songs (artist, title)
+    VALUES ("Queen", "Bohemian Rhapsody");
+INSERT INTO songs (artist, title)
+    VALUES ("Guns N' Roses", "Don't cry");
+
+/**Select the title of all the songs by the artist named 'Queen'.**/
+SELECT title FROM songs WHERE artist = 'Queen';
+
+/**Select the name of all of the artists from the 'Pop' genre**/
+SELECT name FROM artists WHERE genre = 'Pop';
+
+/**Creating the 'Pop' playlist, add another query that will select the title of all the songs from the 'Pop' artists.**/
+SELECT title FROM songs WHERE artist IN (SELECT name FROM artists WHERE genre = 'Pop');
+
+/**Select all the authors who have written more than 1 million words,
+using GROUP BY and HAVING. Your results table should include the 'author' and their total word count as a 'total_words' column.**/    
+SELECT author, SUM(words) AS total_words FROM books 
+GROUP BY author HAVING total_words > 1000000;
+
+/**Select all the authors that write more than an average of 150,000 words per book. 
+Your results table should include the 'author' and average words as an 'avg_words' column.**/
+SELECT author, AVG(words) AS avg_words FROM books 
+GROUP BY author HAVING avg_words > 150000;
